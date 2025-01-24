7b17ff9157daddd7ba4db0a3651211a472ee698e
DIEUDONNE NIONGRE <diniongre@gmail.com>
2025-01-24 17:18:40 +0000

    Personnalisation de l'affichage des articles

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -3,30 +3,32 @@
 @section('title', 'Blog')
 
 @section('content')
-<div class="container mt-4 d-flex flex-row">
-
-    <div class="col-l-7">
-        <h1 class="my-4">Tous les Articles</h1>
-        @if($posts->count())
-        <div class="list-group">
-            @foreach($posts as $post)
-            <div class="m-2">
-                <a href="{{ route('posts.show', $post) }}" class="list-group-item list-group-item-action">
-                    <h5 class="mb-1">{{ $post->title }}</h5>
-                    <img src="{{ asset('storage/' .$post->picture) }}" alt="Image de couverture">
-                    <p class="mb-1">{{ Str::limit($post->content, 100) }}</p>
-                    <small>Publié le {{ $post->created_at->format('d/m/Y') }}</small>
-                </a>
-            </div>
-            @endforeach
+<div class="container-fluid mt-4">
+    <div class="row">
+        <!-- Colonne principale pour les articles -->
+        <div class="col-md-8">
+            <h1 class="my-4 p-3 bg-primary text-white fw-bold text-center">Tous les Articles</h1>
+            @if($posts->count())
+                <div class="d-grid gap-4" style="grid-template-columns: repeat(auto-fit, minmax(250px, 1fr)); max-height: 500px; overflow-y: auto;">
+                    @foreach($posts as $post)
+                        <div class="card p-2">
+                            <a href="{{ route('posts.show', $post) }}" class="list-group-item list-group-item-action">
+                                <h5 class="mb-1 text-center text-info  bg-dark p-2">{{ $post->title }}</h5>
+                                <!-- Image responsive -->
+                                <img src="{{ asset('storage/' . $post->picture) }}" alt="Image de couverture" class="img-fluid mb-2">
+                                <p class="mb-1 mt-2">{{ Str::limit($post->content, 200) }}</p>
+                                <small class="flex-end">Publié le {{ $post->created_at->format('d/m/Y') }}</small>
+                            </a>
+                        </div>
+                    @endforeach
+                </div>
+            @else
+                <p>Aucun article trouvé.</p>
+            @endif
         </div>
-        @else
-        <p>Aucun article trouvé.</p>
-        @endif
-    </div>
 
-    <div>
-        <div>
+        <!-- Colonne pour les événements passés -->
+        <div class="col-md-4">
             <h2>Événements Passés</h2>
             <ul class="list-group">
                 <li class="list-group-item">
